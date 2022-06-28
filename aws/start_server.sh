22ea214ff509196d056c2fa93a0f1ec993ba5fd3
Ajeesh Joshy <jishcem@gmail.com>
2022-06-28 10:50:29 +0530

    Update actions

--- aws/start_server.sh
+++ aws/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo npm install --prefix /home/ubuntu/Code/express-typescript-actions
-npm run build --prefix /home/ubuntu/Code/express-typescript-actions
-node /home/ubuntu/Code/express-typescript-actions/dist/index.js
+cd /home/ubuntu/Code/express-typescript-actions
+node dist/index.js
