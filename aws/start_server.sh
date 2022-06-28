7a7bd5aa65cc551b055692c4aba62a2ddda2cdc0
Ajeesh Joshy <jishcem@gmail.com>
2022-06-28 15:41:54 +0530

    Update actions

--- aws/start_server.sh
+++ aws/start_server.sh
@@ -3,4 +3,4 @@
 sudo npm install --prefix /home/ubuntu/Code/express-typescript-actions
 cd /home/ubuntu/Code/express-typescript-actions
 npm run build
-sudo systemctl stop node-express.service
+sudo systemctl start node-express.service
